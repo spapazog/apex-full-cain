353ffe0c84746c3ad64478492d1aec639041dfc0
Stephane Papazoglou <spapazog@gmail.com>
2023-12-19 12:39:39 +0100

    Comply with codenito architecture

--- api/config.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-printf '%s\n\n' 'Content-type: text/html'
-
-curl "http://www.apex-timing.com/live-timing/$QUERY_STRING/javascript/config.js"
--- api/request.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-printf '%s\n\n' 'Content-type: text/html'
-
-curl -X POST http://www.apex-timing.com/live-timing/commonv2/functions/request.php -H "Content-Type: application/x-www-form-urlencoded" -d $QUERY_STRING
